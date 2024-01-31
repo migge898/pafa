afc6764be994497b6ab369a898f38cfd152a0ef0
Miguel M <50322006+migge898@users.noreply.github.com>
2024-01-31 20:45:44 +0100

    Working react-agent using instructor

--- pafa/instructor_openai.py
+++ pafa/instructor_openai.py
@@ -1,11 +1,12 @@
+import json
 import instructor
 from openai import OpenAI
 from pydantic import Field
 from instructor import OpenAISchema
-from typing import Optional, Union
+from typing import Dict, List
 from enum import Enum
 
-from pafa.react.prompt import ReAct_Prompt
+from pafa.react.prompt import ReAct_Answer_Prompt, ReAct_Prompt
 
 # Enables `response_model`
 client = instructor.patch(OpenAI())
@@ -26,76 +27,142 @@ def get_magic_words(sentence):
     return magic_word_string
 
 
-####
+class States(Enum):
+    """States available for the agent."""
+
+    QUESTION = "QUESTION"
+    THOUGHT = "THOUGHT"
+    ACTION = "ACTION"
+    OBSERVATION = "OBSERVATION"
+    FINAL_ANSWER = "FINAL_ANSWER"
+
+
+class Thought(OpenAISchema):
+    """Thought schema contains text for the thought and a boolean indicating if the final answer can be reached"""
+
+    thought: str
+    reached_final_answer: bool
+
+
+class FinalAnswer(OpenAISchema):
+    """With the help of history, answer the question of the beginning in a text."""
+
+    final_answer: str
 
 
 class Actions(Enum):
     """Actions available for the agent."""
 
-    GET_MAGIC_WORDS = "GET_MAGIC_WORDS"
-    GET_NUM_VOWELS = "GET_NUM_VOWELS"
+    get_magic_words = "get_magic_words"
+    get_num_vowels = "get_num_vowels"
 
 
 class Action(OpenAISchema):
     """Schema for Action takes the name of the action
-    as action_type and argument for that action as action_parameter
+    as action_type and the corresponding parameter as action_parameter
     """
 
     action_type: Actions
     action_parameter: str
 
 
-class ResponseObject(OpenAISchema):
-    """You can either have a thought or an action or a final answer"""
-
-    thought_or_action: Optional[Union[str, Action]]
-    final_answer: Optional[str] = Field(
-        ..., description="Final answer if question can be answered by you."
-    )
-
-
-history = """"""
-question = "`How many vowels are in the first magic word of this sentence: `Hello, please help me my wauwau`"
-history += f"Question: `{question}`"
-system_message = {"role": "system", "content": ReAct_Prompt}
-cnt = 0
-
-while cnt < 10:
+def chat_completion_request(
+    message: str,
+    system_prompt: str = ReAct_Prompt,
+    model: str = "gpt-3.5-turbo",
+    response_model: OpenAISchema = None,
+):
     user_message = {
         "role": "user",
-        "content": history,
+        "content": message,
+    }
+
+    system_message = {
+        "role": "system",
+        "content": system_prompt,
     }
 
     messages = [system_message, user_message]
 
-    print(f"Beginning iteration {cnt}...")
-
-    resp: ResponseObject = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        response_model=ResponseObject,
+    return client.chat.completions.create(
+        model=model,
+        response_model=response_model,
         max_retries=2,
         messages=messages,
     )
 
-    if resp.final_answer:
-        break
-    elif resp.thought_or_action:
-        if isinstance(resp.thought_or_action, str):
-            thought = resp.thought_or_action
-            history += f"\nThought: {thought}"
-        elif isinstance(resp.thought_or_action, Action):
-            action: Action = resp.thought_or_action
-            if action.action_type == Actions.GET_MAGIC_WORDS:
-                history += f"\nGet magic words of sentence: {action.action_parameter}"
+
+def update_usage_stats(
+    resp: OpenAISchema, history: str, call_id: str, usage_stats: Dict
+):
+    raw_resonse = resp._raw_response.usage
+    if call_id not in usage_stats:
+        usage_stats[call_id] = {}
+        usage_stats[call_id]["history"] = history
+        usage_stats[call_id]["input_token"] = raw_resonse.prompt_tokens
+        usage_stats[call_id]["output_token"] = raw_resonse.completion_tokens
+        usage_stats[call_id]["total_tokens"] = raw_resonse.total_tokens
+
+    # cost calculation from https://openai.com/pricing using gpt-3.5-turbo-1106
+    usage_stats["total_input_tokens"] = sum(
+        [usage_stats[call_id]["input_token"] for call_id in usage_stats]
+    )
+    usage_stats["total_output_tokens"] = sum(
+        [usage_stats[call_id]["output_token"] for call_id in usage_stats]
+    )
+    usage_stats["total_tokens"] = sum(
+        [usage_stats[call_id]["total_tokens"] for call_id in usage_stats]
+    )
+    usage_stats["estimated_cost_dollar"] = (
+        usage_stats["total_input_tokens"] * 0.0010
+        + usage_stats["total_output_tokens"] * 0.002
+    )
+
+
+current_state = States.QUESTION
+history = """"""
+question = "`How many vowels are in the first magic word of this sentence: `Hello, please help me my wauwau`"
+system_message = {"role": "system", "content": ReAct_Prompt}
+cnt = 0
+usage_stats = {}
+
+while cnt < 10:
+    match current_state:
+        case States.QUESTION:
+            history += f"Question: `{question}`"
+            current_state = States.THOUGHT
+        case States.THOUGHT:
+            thought: Thought = chat_completion_request(history, response_model=Thought)
+            update_usage_stats(thought, history, f"thought{cnt}", usage_stats)
+            if thought.reached_final_answer:
+                history += f"\nThought: {thought.thought}\nFinal Answer:"
+                current_state = States.FINAL_ANSWER
+            else:
+                history += f"\nThought: {thought.thought}"
+                current_state = States.ACTION
+        case States.ACTION:
+            action: Action = chat_completion_request(history, response_model=Action)
+            update_usage_stats(action, history, f"action{cnt}", usage_stats)
+            if action.action_type == Actions.get_magic_words:
+                history += f"\nAction: Call get_magic_words with parameter: {action.action_parameter}"
                 magic_words = get_magic_words(action.action_parameter)
-                history += f"\Observation: Magic words: {magic_words}"
-            elif action.action_type == Actions.GET_NUM_VOWELS:
-                history += f"\nGet number of vowels in word: {action.action_parameter}"
+                history += f"\nObservation: {magic_words}"
+            elif action.action_type == Actions.get_num_vowels:
+                history += f"\nAction: Call get_num_vowels with parameter: {action.action_parameter}"
                 num_vowels = get_num_vowels(action.action_parameter)
-                history += f"\Observation: Number of vowels: {num_vowels}"
-    else:
-        raise Exception("Invalid response from GPT-3")
+                history += f"\nObservation: {num_vowels}"
+            current_state = States.OBSERVATION
+        case States.OBSERVATION:
+            current_state = States.THOUGHT
+        case States.FINAL_ANSWER:
+            # final_answer: FinalAnswer = chat_completion_request(
+            #     history, response_model=FinalAnswer, system_prompt=ReAct_Answer_Prompt
+            # )
+            # update_usage_stats(final_answer, history, f"final_answer{cnt}", usage_stats)
+            print(f"History:\n{history}")
+
+            with open("usage_stats.json", "w") as f:
+                json.dump(usage_stats, f, indent=4)
+            break
 
     cnt += 1
-
-print(history)
