66159a969c67d5c8a4b113bee421a2884d0c27b2
Miguel M <50322006+migge898@users.noreply.github.com>
2024-01-31 21:01:37 +0100

    removed debug key

--- pafa/config/config.py
+++ pafa/config/config.py
@@ -5,6 +5,3 @@ load_dotenv()
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 MS_TEAMS_WEBHOOK_URL = os.environ.get("MS_TEAMS_WEBHOOK_URL")
-PINECONE_ENV = os.environ.get("PINECONE_ENV")
-PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
-TEST = os.environ.get("TEST")
