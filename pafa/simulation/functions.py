6fc8f0ce4f1826d1792b2adb410dbd00f92016c7
Miguel M <50322006+migge898@users.noreply.github.com>
2024-02-02 16:13:24 +0100

    logic for bugticket service gen implemented

--- pafa/simulation/functions.py
+++ pafa/simulation/functions.py
@@ -118,6 +118,54 @@ def generate_pod_log(
             timestamps,
         )
         return list(timestamps)
+    def generate_level_message_pairs(pod_kind: str):
+        result = []
+        # 90% of the time, the number of tickets is between 10 and 30, otherwise it's between 31 and 100
+        ticket_num = lambda: random.randint(10, 30) if random.random() < 0.9 else random.randint(31, 100)
+        tag = lambda: random.choice("paymentservice", "client", "launcher", "misc")
+        # cpu over 10, 20 or 30
+        cpu_usage = lambda: random.choice([10, 20, 30])
+        infos = lambda x: filter(lambda x: x[0] == "INFO", x)
+        warns = lambda x: filter(lambda x: x[0] == "WARN", x)
+        # returns a list of weights for the pairings like: 3 * [0.95] + 2 * [0.05] = [0.95, 0.95, 0.95, 0.05, 0.05]
+        weights_gen = lambda pairings, warn_weight: len(list(infos(pairings))) * [(1 - warn_weight)] + len(list(warns(pairings))) * [warn_weight]
+        match pod_kind:
+            case "bugticketsvc":
+                pairings = [
+                    ("INFO", f"{ticket_num()} new open tickets for tag: {tag()}"),
+                    ("INFO", f"{ticket_num()} tickets have been closed for tag: {tag()}"),
+                    ("INFO", f"{ticket_num()// 3} tickets have been reopened for tag: {tag()}"),
+                    ("INFO", f"{ticket_num()// 2} tickets have been assigned for tag: {tag()}"),
+                    ("INFO", f"{ticket_num()} tickets have been unassigned for tag: {tag()}"),
+                    ("INFO", f"{ticket_num()} tickets have been resolved for tag: {tag()}"),
+                    ("WARN", f"{ticket_num()// 5} tickets have not been resolved for more than 3 days: {tag()}"),
+                    ("WARN", f"CPU usage of this pod over {cpu_usage()}%."),
+                ]
+                result = random.choices(pairings, k=num_rows, weights=weights_gen(pairings, 0.05))
+            case "paymentsvc":
+                pass
+            case "userauthsvc":
+                pass
+            case "messagersvc":
+                pass
+            case "playerstatssvc":
+                pass
+
+    # bugticketsvc-main-0  ->  bugticketsvc
+    pod_kind = pod.split("-")[0]
+    assert pod_kind in [
+        "bugticketsvc",
+        "paymentsvc",
+        "userauthsvc",
+        "messagersvc",
+        "playerstatssvc",
+    ]
+    log_entries = []
+    times = generate_timestamps()
+
+    # Log should look like this, but in JSON: t=2024-01-22T18:44:03.692166958Z level=info msg="State cache has been initialized"
+    for i in range(num_rows):
+        log_entries.append({"timestamp": times[i], "level": None, "msg": None})
 
     return generate_timestamps()
 
@@ -168,10 +216,30 @@ if __name__ == "__main__":
     # )
     # generate_namespace("test-000")
 
-    a = generate_pod_log(
-        namespace="test-000",
-        pod="bugticketsvc-main-0",
-        start_time_millis="1706828197476",
-        num_rows=3,
-    )
-    print(a)
+    # a = generate_pod_log(
+    #     namespace="test-000",
+    #     pod="bugticketsvc-main-0",
+    #     start_time_millis="1706828197476",
+    #     num_rows=3,
+    # )
+    ticket_num = lambda: random.randint(10, 30) if random.random() < 0.9 else random.randint(31, 100)
+    tag = lambda: random.choice(["paymentservice", "client", "launcher", "misc"])
+    cpu_usage = lambda: random.choice([10, 20, 30])
+    infos = lambda x: filter(lambda x: x[0] == "INFO", x)
+    warns = lambda x: filter(lambda x: x[0] == "WARN", x)
+    # returns a list of weights for the pairings
+    weights_gen = lambda pairings, warn_weight: len(list(infos(pairings))) * [(1 - warn_weight)] + len(list(warns(pairings))) * [warn_weight]
+    pairings = [
+        ("INFO", f"{ticket_num()} new open tickets for tag: {tag()}"),
+        ("INFO", f"{ticket_num()} tickets have been closed for tag: {tag()}"),
+        ("INFO", f"{ticket_num()// 3} tickets have been reopened for tag: {tag()}"),
+        ("INFO", f"{ticket_num()// 2} tickets have been assigned for tag: {tag()}"),
+        ("INFO", f"{ticket_num()} tickets have been unassigned for tag: {tag()}"),
+        ("INFO", f"{ticket_num()} tickets have been resolved for tag: {tag()}"),
+        ("WARN", f"{ticket_num()// 5} tickets have not been resolved for more than 3 days: {tag()}"),
+        ("WARN", f"CPU usage of this pod over {cpu_usage()}%."),
+    ]
+    # result = random.choices(pairings, k=30, weights=[0.95, 0.05])
+    # [print(element) for element in result]
+    print(weights_gen(pairings, 0.05))
+    # filter pairings for warn
