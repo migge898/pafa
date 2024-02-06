588cafe40260f80037e419a2fbbbc22c8fb90851
Miguel M <50322006+migge898@users.noreply.github.com>
2024-02-06 13:10:00 +0100

    alert setup ready

--- pafa/simulation/experiment.py
+++ pafa/simulation/experiment.py
@@ -11,6 +11,9 @@ def init_namespaces():
     generate_namespace("test-005")
     pass
 def init_alerts():
+    pairs = []
+    pairs.append(("bug_s1", "BugTicketServiceOverloaded"))
+
     # alerts for bugticketsvc
     generate_alert(
         description="BugTicketservice in namespace test-001 is using 90% of CPU.",
@@ -18,35 +21,44 @@ def init_alerts():
         namespace="test-001",
         pod="bugticketsvc-main-1",
         active_at_time="2024-02-06T11:09:02.330354Z"
-    )
+    )#bug_l1
+    pairs.append(("bug_l1", "BugTicketServiceOverloaded2"))
+
     generate_alert(
         description="BugTicketservice has exceeded 300 open tickets.",
         alertname="TicketCountHigh",
         namespace="test-001",
         pod="bugticketsvc-main-2",
         active_at_time="2024-02-06T11:09:02.330354Z"
-    )
+    )#bug_l2
+    pairs.append(("bug_l2", "TicketCountHigh"))
+
     generate_alert(
         description="BugTicketservice in namespace test-002 is using 90% of CPU.",
         alertname="BugTicketServiceOverloaded3",
         namespace="test-002",
         pod="bugticketsvc-main-0",
         active_at_time="2024-02-06T09:40:06.331935Z"
-    )
+    )#bug_s3
+    pairs.append(("bug_s3", "BugTicketServiceOverloaded3"))
+
     generate_alert(
         description="BugTicketservice has exceeded 300 open tickets.",
         alertname="TicketCountHigh2",
         namespace="test-002",
         pod="bugticketsvc-main-1",
         active_at_time="2024-02-06T08:40:06.331935Z"
-    )
+    )#bug_s2
+    pairs.append(("bug_s2", "TicketCountHigh2"))
+
     generate_alert(
         description="BugTicketservice has exceeded 300 open tickets.",
         alertname="TicketCountHigh3",
         namespace="test-003",
         pod="bugticketsvc-main-0",
         active_at_time="2024-02-06T08:40:06.331935Z"
-    )
+    )#bug_s4
+    pairs.append(("bug_s4", "TicketCountHigh3"))
     # alerts for paymentsvc
     generate_alert(
         description="PaymentService in namespace test-000 not available.",
@@ -54,28 +66,36 @@ def init_alerts():
         namespace="test-000",
         pod="paymentsvc-main-0",
         active_at_time="2024-02-04T19:22:47.862156Z"
-    )
+    )#pay_l1
+    pairs.append(("pay_l1", "PaymentServiceDown"))
+
     generate_alert(
         description="PaymentService in namespace test-001 is using 70% of CPU.",
         alertname="PaymentServiceOverloaded",
         namespace="test-002",
         pod="paymentsvc-main-0",
         active_at_time="2024-02-05T22:57:37.332298Z"
-    )
+    )#pay_l2
+    pairs.append(("pay_l2", "PaymentServiceOverloaded"))
+
     generate_alert(
         description="No transactions from America-North in last 30 minutes.",
         alertname="NoTransactionsFromRegion",
         namespace="test-003",
         pod="paymentsvc-main-0",
         active_at_time="2024-02-06T12:00:51.333993Z"
-    )
+    )#pay_s1
+    pairs.append(("pay_s1", "NoTransactionsFromRegion"))
+
     generate_alert(
         description="More than 500 payments refunded in last 30 minutes.",
         alertname="RefundSpam",
         namespace="test-003",
         pod="paymentsvc-main-1",
         active_at_time="2024-02-05T20:46:16.334115Z"
-    )
+    )#pay_s2
+    pairs.append(("pay_s2", "RefundSpam"))
+
     # alerts for userauthsvc
     generate_alert(
         description="UserAuthService offline in namespace test-002.",
@@ -83,14 +103,18 @@ def init_alerts():
         namespace="test-002",
         pod="userauthsvc-main-0",
         active_at_time="2024-02-06T03:58:51.852511Z"
-    )
+    )#auth_l2
+    pairs.append(("auth_l2", "UserAuthServiceDown"))
+
     generate_alert(
         description="More than 20000 users of America-North cannot login.",
         alertname="AuthServiceDownForRegion",
         namespace="test-003",
         pod="userauthsvc-main-0",
         active_at_time="2024-02-05T23:45:53.334234Z"
-    )
+    )#auth_l1
+    pairs.append(("auth_l1", "AuthServiceDownForRegion"))
+
     # alerts for messengersvc
     generate_alert(
         description="Last broadcast message promoted item for 0$",
@@ -98,57 +122,74 @@ def init_alerts():
         namespace="test-003",
         pod="messengersvc-main-0",
         active_at_time="2024-02-05T22:50:27.334353Z"
-    )
+    )#msg_l1
+    pairs.append(("msg_l1", "ZeroCostAlert"))
+
     generate_alert(
         description="More than 5 broadcasts with the same message were sent in last 30 minutes.",
         alertname="MessageSpam",
         namespace="test-003",
         pod="messengersvc-main-1",
         active_at_time="2024-02-06T08:10:02.334472Z"
-    )
+    )#msg_l2
+    pairs.append(("msg_l2", "MessageSpam"))
+
     generate_alert(
         description="No messages received from region: America-North in last 60 minutes.",
         alertname="NoMessagesFromRegion",
         namespace="test-003",
         pod="messengersvc-main-2",
         active_at_time="2024-02-06T03:32:32.334591Z"
-    )
+    )#msg_s2
+    pairs.append(("msg_s2", "NoMessagesFromRegion"))
+
     generate_alert(
         description="Messengerservice received messages below 30% of the average.",
         alertname="MessagesBelowAverage",
         namespace="test-004",
         pod="messengersvc-main-0",
         active_at_time="2024-02-05T21:37:21.336132Z"
-    )
+    )#msg_s1
+    pairs.append(("msg_s1", "MessagesBelowAverage"))
+
     # alerts for playerstatssvc
     generate_alert(
-        description="Failed to load statistcs.",
+        description="Failed to load some statistics.",
         alertname="PlayerStatsMissing",
         namespace="test-002",
         pod="playerstatssvc-main-0",
         active_at_time="2024-02-06T10:33:38.333018Z"
-    )
+    )#play_s1
+    pairs.append(("play_s1", "PlayerStatsMissing"))
+
     generate_alert(
         description="Too many players affected by configuration change.",
         alertname="TooManyPlayersAffected",
         namespace="test-004",
         pod="playerstatssvc-main-0",
         active_at_time="2024-02-06T04:45:45.336485Z"
-    )
+    )#play_l2
+    pairs.append(("play_l2", "TooManyPlayersAffected"))
+
     generate_alert(
         description="PlayerStatsService in namespace test-002 is down.",
         alertname="PlayerStatsServiceDown",
         namespace="test-002",
         pod="playerstatssvc-main-2",
         active_at_time="2024-02-06T02:42:57.333252Z"
-    )
+    )#play_l1
+    pairs.append(("play_l1", "PlayerStatsServiceDown"))
+
     generate_alert(
         description="Users checked stats below 30% of the average.",
         alertname="StatsCheckBelowAverage",
         namespace="test-002",
         pod="playerstatssvc-main-1",
         active_at_time="2024-02-06T06:18:17.333135Z"
-    )
+    )#play_s2
+    pairs.append(("play_s2", "StatsCheckBelowAverage"))
+
+    return pairs
 
 
 def init_data():
